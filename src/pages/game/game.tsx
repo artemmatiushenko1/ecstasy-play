4a46333f35c2ea40b060ba1595ae937311dbcc28
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-12-16 18:54:51 +0200

    gc-00: * fix game progress values design

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,5 +1,5 @@
 import { ErrorBoundary } from '@/libs/components/components.js';
-import { Button, Spinner, Tooltip } from '@nextui-org/react';
+import { Button, Chip, Spinner, Tooltip } from '@nextui-org/react';
 import { Suspense, lazy, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
@@ -63,49 +63,77 @@ const GamePage = () => {
     setHasGameEnded(false);
   };
 
-  const appsMap = {
-    [GameApp.SNAKE]: SnakeApp,
-    [GameApp.CONNECT_TILES]: ConnectTilesApp,
-    [GameApp.TETRIS]: TetrisApp,
+  const appsDataMapper = {
+    [GameApp.SNAKE]: {
+      name: 'Snake',
+      Component: SnakeApp,
+    },
+    [GameApp.CONNECT_TILES]: {
+      name: 'Connect Tiles',
+      Component: ConnectTilesApp,
+    },
+    [GameApp.TETRIS]: {
+      name: 'Tetris',
+      Component: TetrisApp,
+    },
   };
 
-  const GameAppComponent =
-    appsMap[location.state.appId as keyof typeof appsMap];
+  const { Component: GameAppComponent, name: appName } =
+    appsDataMapper[location.state.appId as keyof typeof appsDataMapper];
 
   return (
     <>
       <div className="flex flex-col h-full">
-        <div className="w-full flex justify-between">
-          <Tooltip
-            placement="bottom"
-            content={
-              <div className="text-center">
-                Please note that your progress will not be saved
-                <br /> once you quit the game.
-              </div>
-            }
-          >
-            <Button
-              color="danger"
-              variant="ghost"
-              onPress={handleQuitGame}
-              startContent={<MdOutlineArrowBackIosNew />}
+        <div className="w-full flex justify-between items-center">
+          <div className="flex-1 self-center">
+            <Tooltip
+              placement="bottom"
+              content={
+                <div className="text-center">
+                  Please note that your progress will not be saved
+                  <br /> once you quit the game.
+                </div>
+              }
             >
-              Quit Game
-            </Button>
-          </Tooltip>
-          <div className="flex items-center gap-5">
+              <Button
+                color="danger"
+                variant="solid"
+                onPress={handleQuitGame}
+                startContent={<MdOutlineArrowBackIosNew />}
+              >
+                Quit Game
+              </Button>
+            </Tooltip>
+          </div>
+          <h1 className="py-2 px-4 rounded-lg text-lg font-medium flex-1 text-center">
+            {appName}
+          </h1>
+          <div className="flex items-center justify-end gap-5 flex-1">
             <Tooltip content="Score" placement="bottom">
-              <p className="font-bold text-xl inline-flex items-center gap-2 text-default-400">
+              <Chip
+                color="primary"
+                className="text-xl"
+                classNames={{
+                  base: 'h-auto bg-primary-50',
+                  content: 'flex items-center gap-2 text-primary font-medium',
+                }}
+              >
                 <MdSportsScore />
                 <span>{score}</span>
-              </p>
+              </Chip>
             </Tooltip>
             <Tooltip content="Playing Time" placement="bottom">
-              <p className="font-bold text-xl inline-flex items-center gap-2 text-default-400">
+              <Chip
+                color="primary"
+                className="text-xl"
+                classNames={{
+                  base: 'h-auto bg-primary-50',
+                  content: 'flex items-center gap-2 text-primary font-medium',
+                }}
+              >
                 <MdAccessTime />
                 <span>00:00</span>
-              </p>
+              </Chip>
             </Tooltip>
           </div>
         </div>
