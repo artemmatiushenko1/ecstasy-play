91dbecda06ce40eaa30ccb4e7bed22ae657c3dca
KhrapkoVasyl <khrapko2002@gmail.com>
2023-12-27 01:03:37 +0200

    chore(server): add use of fastify under the hood of nest

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const HOST = configService.get('NEST_HOST');
+  const PORT = configService.get('NEST_PORT');
+  const GLOBAL_PREFIX = configService.get('GLOBAL_PREFIX');
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  await app.listen(PORT, HOST, () => {
+    console.log(`Server listens on http://${HOST}:${PORT}`);
+  });
 }
+
 bootstrap();
