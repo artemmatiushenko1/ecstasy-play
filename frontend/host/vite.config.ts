8833f2ced9fc03d4abc91def343dde1bb5cd4bab
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-12-27 20:01:23 +0200

    feat(frontend): add env variables

--- frontend/host/vite.config.ts
+++ frontend/host/vite.config.ts
@@ -1,26 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'host-app',
-      shared: ['react', 'react-dom'],
-      remotes: {
-        'connectTiles': 'http://localhost:3001/assets/remoteEntry.js',
-        'snake': 'http://localhost:3002/assets/remoteEntry.js',
-        'tetris': 'http://localhost:3003/assets/remoteEntry.js',
+export default defineConfig(({ mode }) => {
+  const envDir = process.cwd();
+  const env = { ...process.env, ...loadEnv(mode, envDir) };
+
+  console.log(mode, env.VITE_CONNECT_TILES_HOST);
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'host-app',
+        shared: ['react', 'react-dom'],
+        remotes: {
+          'connectTiles': `${env.VITE_CONNECT_TILES_HOST}/assets/remoteEntry.js`,
+          'snake': `${env.VITE_SNAKE_HOST}/assets/remoteEntry.js`,
+          'tetris': `${env.VITE_TETRIS_HOST}/assets/remoteEntry.js`,
+        },
+      }),
+    ],
+    build: { target: 'esnext' },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
-    }),
-  ],
-  build: { target: 'esnext' },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
     },
-  },
+  };
 });
